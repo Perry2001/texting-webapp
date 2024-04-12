cb6fefa16544ed65a91757016148c441f0545c9a
Perry <johnadducul2001@gmail.com>
2024-04-12 11:23:42 +0800

    first commit

---
+++ public/firebaseConfig.js
@@ -0,0 +1,17 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaMIG_ZkCgOI4K6OCLB3ewG0Q_Wht3Z4Q",
+  authDomain: "texting-webapp.firebaseapp.com",
+  databaseURL:
+    "https://texting-webapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "texting-webapp",
+  storageBucket: "texting-webapp.appspot.com",
+  messagingSenderId: "797680318735",
+  appId: "1:797680318735:web:88ca4c88a0aabc46f4a67a",
+  measurementId: "G-L5VG4HNSB3",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
